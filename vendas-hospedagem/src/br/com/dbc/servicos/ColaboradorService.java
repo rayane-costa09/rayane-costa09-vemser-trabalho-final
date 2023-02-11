9e565ee505ab4314fb1d5f20790d21debd45f53e
Marcklen <marcklen@icloud.com>
2023-02-11 10:57:33 -0300

    CRUD colaborador ok

--- vendas-hospedagem/src/br/com/dbc/servicos/ColaboradorService.java
+++ vendas-hospedagem/src/br/com/dbc/servicos/ColaboradorService.java
@@ -1,8 +1,6 @@
 package br.com.dbc.servicos;
 
 import br.com.dbc.modelos.Colaborador;
-import br.com.dbc.modelos.Contato;
-import br.com.dbc.modelos.Endereco;
 import br.com.dbc.repositorios.ICrud;
 
 import java.util.ArrayList;
@@ -13,14 +11,10 @@ public class ColaboradorService implements ICrud {
 
     static Scanner scanner = new Scanner(System.in);
 
-    static List<Colaborador> colaboradores;
-    static List<Contato> contatos;
-    static List<Endereco> enderecos;
+    private List<Colaborador> colaboradores;
 
     public ColaboradorService() {
         colaboradores = new ArrayList<>();
-        contatos = new ArrayList<>();
-        enderecos = new ArrayList<>();
     }
 
     @Override
@@ -33,25 +27,55 @@ public class ColaboradorService implements ICrud {
         String usuario = scanner.nextLine();
         System.out.println("Digite a senha do colaborador: ");
         String senha = scanner.nextLine();
-        System.out.println("Informe os dados de contato do colaborador: ");
-//        colaboradores.setContatos.add(new Contato());
-//        colaboradores.setEnderecos.add(new Endereco());
         colaboradores.add(new Colaborador(nome, cpf, usuario, senha));
         System.out.println("Colaborador " + nome + " cadastrado com sucesso!");
     }
 
     @Override
     public void read() {
-
+        if (colaboradores.isEmpty()) {
+            System.out.println("Lista Vazia -- Não há colaboradores cadastrados!");
+        } else {
+            for (Colaborador colaborador : colaboradores) {
+                System.out.println(colaborador);
+            }
+        }
     }
 
     @Override
     public void update() {
-
+        System.out.println("Atualizando colaborador por nome...");
+        System.out.println("Digite o nome do colaborador: ");
+        String nome = scanner.nextLine();
+        for (Colaborador colaborador : colaboradores ) {
+            if (colaborador.getNome().equalsIgnoreCase(nome)) {
+                System.out.println("Digite o nome do colaborador: ");
+                String novoNome = scanner.nextLine();
+                System.out.println("Digite o cpf do colaborador: ");
+                String cpf = scanner.nextLine();
+                System.out.println("Digite o usuario do colaborador: ");
+                String usuario = scanner.nextLine();
+                System.out.println("Digite a senha do colaborador: ");
+                String senha = scanner.nextLine();
+                colaborador.setNome(novoNome);
+                colaborador.setCpf(cpf);
+                colaborador.setUsuario(usuario);
+                colaborador.setSenha(senha);
+                System.out.println("Colaborador " + nome + " atualizado com sucesso!");
+            }
+        }
     }
 
     @Override
     public void delete() {
-
+        System.out.println("Deletando colaborador por nome...");
+        System.out.println("Digite o nome do colaborador: ");
+        String nome = scanner.nextLine();
+        for (Colaborador colaborador : colaboradores ) {
+            if (colaborador.getNome().equalsIgnoreCase(nome)) {
+                colaboradores.remove(colaborador);
+                System.out.println("Colaborador " + nome + " deletado com sucesso!");
+            }
+        }
     }
 }
